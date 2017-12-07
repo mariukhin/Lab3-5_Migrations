48fadd4aaa9d319a5ad67c0b8c8208fba37a366f
maxim_mariukhin <maryuhin.z@gmail.com>
2017-12-07 19:26:20 +0200

    modified some files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,42 +25,86 @@ public class Main {
 
         Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
 
-        File change_create_tables = new File(ClassLoader.getSystemResource("changelog/deleteAllTable/db.changelog-0.0.0.xml").getFile());
-        File change_set_data = new File(ClassLoader.getSystemResource("liquibase/3.0-set-data.xml").getFile());
-        File change_set_row = new File(ClassLoader.getSystemResource("liquibase/3.1-set-row.xml").getFile());
-        File change_update_row = new File(ClassLoader.getSystemResource("liquibase/3.2-update-row.xml").getFile());
-        File change_join = new File(ClassLoader.getSystemResource("liquibase/3.3-query-join.xml").getFile());
-        File change_rename = new File(ClassLoader.getSystemResource("liquibase/5.0-rename-column.xml").getFile());
-        File change_new_column = new File(ClassLoader.getSystemResource("liquibase/5.1-new-column.xml").getFile());
-        File change_new_table = new File(ClassLoader.getSystemResource("liquibase/5.2-new-table.xml").getFile());
-        File change_delete_row = new File(ClassLoader.getSystemResource("liquibase/4.1-delete-row.xml").getFile());
-        File change_delete_rows = new File(ClassLoader.getSystemResource("liquibase/4.0-delete-all-rows.xml").getFile());
-        File change_drop_tables = new File(ClassLoader.getSystemResource("liquibase/2.0-drop-tables.xml").getFile());
+        File change_create_tableFirst = new File(ClassLoader.getSystemResource("changelog/create/db.changelog-1.0.1.xml").getFile());
+        File change_create_tableSecond = new File(ClassLoader.getSystemResource("changelog/create/db.changelog-1.0.2.xml").getFile());
+        File change_create_tableThird = new File(ClassLoader.getSystemResource("changelog/create/db.changelog-1.0.3.xml").getFile());
+        File change_create_tableFourth = new File(ClassLoader.getSystemResource("changelog/create/db.changelog-1.0.4.xml").getFile());
+        File change_create_tableFifth = new File(ClassLoader.getSystemResource("changelog/create/db.changelog-1.0.5.xml").getFile());
 
-        Liquibase create_tables = new liquibase.Liquibase(change_create_tables.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase set_data = new liquibase.Liquibase(change_set_data.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase set_row = new liquibase.Liquibase(change_set_row.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase update_row = new liquibase.Liquibase(change_update_row.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase query_join = new liquibase.Liquibase(change_join.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase rename_column = new liquibase.Liquibase(change_rename.toString(), new FileSystemResourceAccessor(), database);
+        File change_insert_tableFirst = new File(ClassLoader.getSystemResource("changelog/insert/db.changelog-1.1.1.xml").getFile());
+        File change_insert_tableSecond = new File(ClassLoader.getSystemResource("changelog/insert/db.changelog-1.1.2.xml").getFile());
+        File change_insert_tableThird = new File(ClassLoader.getSystemResource("changelog/insert/db.changelog-1.1.3.xml").getFile());
+        File change_insert_tableFourth = new File(ClassLoader.getSystemResource("changelog/insert/db.changelog-1.1.4.xml").getFile());
+        File change_insert_tableFifth = new File(ClassLoader.getSystemResource("changelog/insert/db.changelog-1.1.5.xml").getFile());
+
+        File change_setRaw = new File(ClassLoader.getSystemResource("changelog/setRaw/db.changelog-1.2.3.xml").getFile());
+        File change_update_table = new File(ClassLoader.getSystemResource("changelog/updateTable/db.changelog-1.5.5.xml").getFile());
+        File change_join = new File(ClassLoader.getSystemResource("changelog/join/db.changelog-2.1.2.xml").getFile());
+        File change_rename_column = new File(ClassLoader.getSystemResource("changelog/renameColumn/db.changelog-1.3.5.xml").getFile());
+        File change_new_column = new File(ClassLoader.getSystemResource("changelog/deleteColumn/db.changelog-1.2.5.1.xml").getFile());
+        File change_new_table = new File(ClassLoader.getSystemResource("changelog/dropTable/db.changelog-1.4.5.1.xml").getFile());
+        File change_delete_row = new File(ClassLoader.getSystemResource("changelog/deleteRow/db.changelog-1.6.5.xml").getFile());
+        File change_deleteAllrows = new File(ClassLoader.getSystemResource("changelog/deleteAllraws/db.changelog-1.3.8.xml").getFile());
+        File change_droptableFirst = new File(ClassLoader.getSystemResource("changelog/dropTableWithoutSaving/db.changelog-1.7.1.xml").getFile());
+        File change_droptableSecond = new File(ClassLoader.getSystemResource("changelog/dropTableWithoutSaving/db.changelog-1.7.2.xml").getFile());
+        File change_droptableThird = new File(ClassLoader.getSystemResource("changelog/dropTableWithoutSaving/db.changelog-1.7.3.xml").getFile());
+        File change_droptableFourth = new File(ClassLoader.getSystemResource("changelog/dropTableWithoutSaving/db.changelog-1.7.4.xml").getFile());
+        File change_droptableFifth = new File(ClassLoader.getSystemResource("changelog/dropTableWithoutSaving/db.changelog-1.7.5.xml").getFile());
+
+
+        Liquibase create_tableFirst = new liquibase.Liquibase(change_create_tableFirst.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase create_tableSecond = new liquibase.Liquibase(change_create_tableSecond.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase create_tableThird = new liquibase.Liquibase(change_create_tableThird.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase create_tableFourth = new liquibase.Liquibase(change_create_tableFourth.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase create_tableFifth = new liquibase.Liquibase(change_create_tableFifth.toString(), new FileSystemResourceAccessor(), database);
+
+        Liquibase insert_tableFirst = new liquibase.Liquibase(change_insert_tableFirst.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase insert_tableSecond = new liquibase.Liquibase(change_insert_tableSecond.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase insert_tableThird = new liquibase.Liquibase(change_insert_tableThird.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase insert_tableFourth = new liquibase.Liquibase(change_insert_tableFourth.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase insert_tableFifth = new liquibase.Liquibase(change_insert_tableFifth.toString(), new FileSystemResourceAccessor(), database);
+
+
+        Liquibase set_raw = new liquibase.Liquibase(change_setRaw.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase update_table = new liquibase.Liquibase(change_update_table.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase full_join = new liquibase.Liquibase(change_join.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase rename_column = new liquibase.Liquibase(change_rename_column.toString(), new FileSystemResourceAccessor(), database);
         Liquibase new_column = new liquibase.Liquibase(change_new_column.toString(), new FileSystemResourceAccessor(), database);
         Liquibase new_table = new liquibase.Liquibase(change_new_table.toString(), new FileSystemResourceAccessor(), database);
         Liquibase delete_row = new liquibase.Liquibase(change_delete_row.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase delete_all_rows = new liquibase.Liquibase(change_delete_rows.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase drop_tables = new liquibase.Liquibase(change_drop_tables.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase delete_all_rows = new liquibase.Liquibase(change_deleteAllrows.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase drop_tableFirst = new liquibase.Liquibase(change_droptableFirst.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase drop_tableSecond = new liquibase.Liquibase(change_droptableSecond.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase drop_tableThird = new liquibase.Liquibase(change_droptableThird.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase drop_tableFourth = new liquibase.Liquibase(change_droptableFourth.toString(), new FileSystemResourceAccessor(), database);
+        Liquibase drop_tableFifth = new liquibase.Liquibase(change_droptableFifth.toString(), new FileSystemResourceAccessor(), database);
 
-        create_tables.update(new Contexts(), new LabelExpression());
-/*        set_data.update(new Contexts(), new LabelExpression());
-        set_row.update(new Contexts(), new LabelExpression());
-        set_row.update(new Contexts(), new LabelExpression());
-        update_row.update(new Contexts(), new LabelExpression());
-        query_join.update(new Contexts(), new LabelExpression());
+        create_tableFirst.update(new Contexts(), new LabelExpression());
+        create_tableSecond.update(new Contexts(), new LabelExpression());
+        create_tableThird.update(new Contexts(), new LabelExpression());
+        create_tableFourth.update(new Contexts(), new LabelExpression());
+        create_tableFifth.update(new Contexts(), new LabelExpression());
+
+        insert_tableFirst.update(new Contexts(), new LabelExpression());
+        insert_tableSecond.update(new Contexts(), new LabelExpression());
+        insert_tableThird.update(new Contexts(), new LabelExpression());
+        insert_tableFourth.update(new Contexts(), new LabelExpression());
+        insert_tableFifth.update(new Contexts(), new LabelExpression());
+
+        set_raw.update(new Contexts(), new LabelExpression());
+        update_table.update(new Contexts(), new LabelExpression());
+        full_join.update(new Contexts(), new LabelExpression());
         rename_column.update(new Contexts(), new LabelExpression());
         new_column.update(new Contexts(), new LabelExpression());
         new_table.update(new Contexts(), new LabelExpression());
         delete_row.update(new Contexts(), new LabelExpression());
         delete_all_rows.update(new Contexts(), new LabelExpression());
-        drop_tables.update(new Contexts(), new LabelExpression());*/
+        drop_tableFirst.update(new Contexts(), new LabelExpression());
+        drop_tableSecond.update(new Contexts(), new LabelExpression());
+        drop_tableThird.update(new Contexts(), new LabelExpression());
+        drop_tableFourth.update(new Contexts(), new LabelExpression());
+        drop_tableFifth.update(new Contexts(), new LabelExpression());
+
 
     }
 }
